8de1ebb121b2a5549e0e3a1b75532d7bad4d885c
komal624 <kb4858@srmist.edu.in>
2026-01-23 14:41:46 +0530

    [Komal] : Added : ZeroSumSubarrays

---
+++ src/DataStructures/HashMaps/ZeroSumSubarrays.java
@@ -0,0 +1,34 @@
+package DataStructures.HashMaps;
+
+import java.util.*;
+
+public class ZeroSumSubarrays {
+
+    public static void findZeroSumSubarrays(int[] arr) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int sum = 0;
+
+        // For sum = 0 at index -1
+        map.put(0, new ArrayList<>());
+        map.get(0).add(-1);
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (map.containsKey(sum)) {
+                for (int startIndex : map.get(sum)) {
+                    System.out.println("Subarray found from index "
+                            + (startIndex + 1) + " to " + i);
+                }
+            }
+
+            map.putIfAbsent(sum, new ArrayList<>());
+            map.get(sum).add(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, -7, 1, 3, -4, -2, -2};
+        findZeroSumSubarrays(arr);
+    }
+}
